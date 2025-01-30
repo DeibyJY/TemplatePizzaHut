8f51c738f3157236df6e0375068450fc52ae5cb4
Jose Alejandro Sahonero Salas <JSAHONERO@farmacorp.com>
2025-01-30 16:49:05 -0400

    hoy

--- assets/theme.js
+++ assets/theme.js
@@ -2432,17 +2432,17 @@
                 text = $('#cart-gift-wrapp').attr('data-add-text'),
                 productLine = $target.data('line');
             
-                console.log('----DEBUG REMOVE CART ITEM----');
-                console.log('Evento:', event);
-                console.log('Target elemento:', $target);
-                console.log('Product ID:', productId);
-                console.log('Text:', text);
-                console.log('Product Line:', productLine);
-                console.log('Target attributes:', {
-                    'data-cart-remove-id': $target.attr('data-cart-remove-id'),
-                    'data-line': $target.data('line'),
-                    'element': $target.get(0)
-            });
+            //     console.log('----DEBUG REMOVE CART ITEM----');
+            //     console.log('Evento:', event);
+            //     console.log('Target elemento:', $target);
+            //     console.log('Product ID:', productId);
+            //     console.log('Text:', text);
+            //     console.log('Product Line:', productLine);
+            //     console.log('Target attributes:', {
+            //         'data-cart-remove-id': $target.attr('data-cart-remove-id'),
+            //         'data-line': $target.data('line'),
+            //         'element': $target.get(0)
+            // });
 
                 $('#cart-gift-wrapp').text(text);
 
