a3675f05a4547e5f660f59acbbba213c14c0814d
Jose Alejandro Sahonero Salas <JSAHONERO@farmacorp.com>
2025-01-30 15:22:25 -0400

    hoy

--- assets/theme.js
+++ assets/theme.js
@@ -1931,6 +1931,7 @@
                 // Si este existe, entonces se ChangeItem en el caso que no AddItem 
                 let itemsCarrito = [];
                 Shopify.getCart(function(cart) {
+                    console.log(cart);
                     itemsCarrito = cart.items;
                     const cuerpoGenerado = Shopify.jsonOpcionesSeleccionadas();
                     
